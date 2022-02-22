070e012525541dc4f99777c3722c440f9b7ccb0f
Benjamin <BensThoughts@Gmail.Com>
2022-02-22 09:36:03 -0700

    changed html tag to label

--- app/components/FormElements/Label.tsx
+++ app/components/FormElements/Label.tsx
@@ -8,11 +8,11 @@ export default function Label({
   tooltipText?: string;
 }) {
   return (
-    <div className="flex gap-2 items-center max-w-[280px] md:max-w-[380px]">
+    <label className="flex gap-2 items-center max-w-[280px] md:max-w-[380px]">
       <span className="align-middle text-neutral-lightest">{label}</span>
       <div>{tooltipText && <Tooltip text={tooltipText} />}</div>
       {/* <DescriptionPopover title="Word" description="The definition of a word" /> */}
       {/* <InformationCircleIcon className='inline-block w-6 h-6 text-icon-secondary'/> */}
-    </div>
+    </label>
   );
 }
