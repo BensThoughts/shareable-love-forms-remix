1a74ef64bd4dfbd279e385bd2f0c945aac64ef86
Benjamin <BensThoughts@Gmail.Com>
2022-02-22 09:34:59 -0700

    added db server functions

--- app/utils/db/db.server.ts
+++ app/utils/db/db.server.ts
@@ -1,3 +1,5 @@
+/* eslint no-var: off, no-unused-vars: off */
+
 import { PrismaClient } from '@prisma/client';
 
 let db: PrismaClient;
@@ -6,6 +8,9 @@ declare global {
   var __db: PrismaClient | undefined;
 }
 
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
 if (process.env.NODE_ENV === 'production') {
   db = new PrismaClient();
   db.$connect();
